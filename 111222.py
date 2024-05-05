0fed6b8c6b8270a13a965cee3cf0f7b89f73bb9b
molly5617 <mollytsai5617@gmail.com>
2024-05-05 22:38:07 +0800

    333

--- 111222.py
+++ 111222.py
@@ -1,10 +1,11 @@
-import sys
 import numpy as np
 
 
 def trinomial_tree_up_and_out_put(S, X, r, s, T, H, n):
+    # p244
+
     delta_t = T / n
-    h = np.log(H / S) // (s * np.sqrt(delta_t))
+    h = np.floor(np.log(H / S) / (s * np.sqrt(delta_t)))
 
     lam = np.log(H / S) / (h * s * np.sqrt(delta_t))
     pu = 1 / (2 * lam**2) + (r - s**2 / 2) * np.sqrt(delta_t) / (2 * lam * s)
@@ -12,6 +13,7 @@ def trinomial_tree_up_and_out_put(S, X, r, s, T, H, n):
     pm = 1 - pu - pd
     u = np.exp(lam * s * np.sqrt(delta_t))
     d = 1 / u
+
     now_prices = [[S]]
     times = [[0]]
 
@@ -20,36 +22,40 @@ def trinomial_tree_up_and_out_put(S, X, r, s, T, H, n):
             now_prices[i][-1],
             now_prices[i][-1] * d,
         ]
-        next_up_times = [times + 1 for times in times[i]] + [
+        next_times = [times + 1 for times in times[i]] + [
             times[i][-1],
             times[i][-1] - 1,
         ]
         now_prices.append(next_prices)
-        times.append(next_up_times)
+        times.append(next_times)
 
+    # 計算到期時的期權價值
     C = [max(0, X - price) if price < H else 0 for price in now_prices[n]]
 
-    for i in range(n - 1, -1, -1):
-        C = [
-            (
-                (pu * C[payoff_i] + pm * C[payoff_i + 1] + pd * C[payoff_i + 2])
-                / np.exp(r * delta_t)
-                if times[i][payoff_i] < h
-                else 0
-            )
-            for payoff_i in range(len(C) - 2)
-        ]
+    # 從到期時刻向後遞推計算期權價值
+    for j in range(n - 1, -1, -1):
+        prev_C = []
+        for i in range(len(C) - 2):
+            if times[j][i] < h:
+                payoff = (pu * C[i] + pm * C[i + 1] + pd * C[i + 2]) / np.exp(
+                    r * delta_t
+                )
+            else:  # 如果觸及障礙，期權價值為0
+                payoff = 0
+            prev_C.append(payoff)
+        C = prev_C
+
     return C[0]
 
 
 if __name__ == "__main__":
-    S = float(sys.argv[1])
-    X = float(sys.argv[2])
-    r = float(sys.argv[3])
-    s = float(sys.argv[4])
-    T = int(sys.argv[5]) / 365
-    H = float(sys.argv[6])
-    n = int(sys.argv[7])
+    S = 100
+    X = 110
+    r = 0.03
+    s = 0.3
+    T = 60 / 365
+    H = 120
+    n = 100
 
     price = trinomial_tree_up_and_out_put(S, X, r, s, T, H, n)
     print("{0:.6f}".format(price))
